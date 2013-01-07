1666fb92bff30f3a27ccc99af5efcd3c53aa9e46
Konstantinos Maragos <kmaragos@gmail.com>
2013-01-07 23:28:30 +0200

    add ultrasonic sensor distance display on LCD

---
+++ UltrasonicLCD/UltrasonicLCD.ino
@@ -0,0 +1,46 @@
+#include <LiquidCrystal.h>
+
+#define trigPin 8
+#define echoPin 9
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  Serial.begin (57000);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  lcd.begin(16, 2);
+  lcd.print("Ultrasonic sensor");
+  Serial.println("Ultrasonic sensor");
+}
+
+void loop() {
+  int duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+
+  lcd.clear();
+  lcd.print("Distance:");
+  lcd.setCursor(0, 1);
+
+  if (distance >= 200 || distance <= 0){
+    lcd.print("Out of range");
+    Serial.println("Out of range");
+  }
+  else {
+    lcd.print(distance);
+    lcd.print(" cm");
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
+
+
+
